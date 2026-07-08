9a8bb2a2673534b25dac20b6a99a70032cec8a90
Argi <argiigmg@gmail.com>
2026-07-08 19:50:19 +0700

    feat: update About section content for clarity and professionalism

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,13 +3,13 @@ const About = () => {
     <section className="mt-10">
       <span className="font-medium dark:text-white">about me</span>
       <p className="text-zinc-500 mt-4">
-        Halo, saya Argi, lulusan Rekayasa Perangkat Lunak yang memiliki keahlian
-        dalam pengembangan website dan aplikasi berbasis web, termasuk HTML,
-        CSS, JavaScript, serta manajemen database. Saya terbiasa menggunakan Git
-        dan memiliki pemahaman dasar mengenai analisis sistem, debugging, dan
-        problem solving. Saya memiliki semangat untuk terus belajar dan
-        mengembangkan kemampuan di bidang software development, serta siap
-        berkontribusi dalam lingkungan profesional.
+        Hello, I’m Argi, a Software Developer graduate with expertise in website
+        and web application development, including HTML, CSS, JavaScript, and
+        database management. I’m proficient in using Git and have a basic
+        understanding of systems analysis, debugging, and problem-solving. I’m
+        passionate about continuously learning and developing my skills in
+        software development, and I’m ready to contribute in a professional
+        environment. Translated with DeepL.com (free version)
       </p>
     </section>
   );
